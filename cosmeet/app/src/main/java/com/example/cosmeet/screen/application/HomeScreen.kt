7c78347068ddf571b8a049fcd7e341baa64a3caa
kanaiama <wilson.kanaiamajr@sptech.school>
2024-04-29 21:33:13 -0300

    tela home

--- cosmeet/app/src/main/java/com/example/cosmeet/screen/application/HomeScreen.kt
+++ cosmeet/app/src/main/java/com/example/cosmeet/screen/application/HomeScreen.kt
@@ -1,15 +1,151 @@
 package com.example.cosmeet.screen.application
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cosmeet.R
 import com.example.cosmeet.screen.login.LoginScreen
 import com.example.cosmeet.ui.theme.CosmeetTheme
+data class Company(val name: String, val description: String)
+
+val mockCompanies = listOf(
+    Company(name = "Nivea", description = "Nivea"),
+    Company(name = "Nivea", description = "Nivea"),
+    Company(name = "Nivea", description = "Nivea"),
+    Company(name = "Nivea", description = "Nivea"),
+    Company(name = "Nivea", description = "Nivea"),
+    Company(name = "Nivea", description = "Nivea"),
+    Company(name = "Nivea", description = "Nivea"),
+    Company(name = "Nivea", description = "Nivea"),
+
+    )
 
 @Composable
 fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, top = 50.dp, end = 20.dp, bottom = 0.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.mipmap.iconteste),
+                    contentDescription = "logo da empresa",
+                    modifier = Modifier.size(80.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Medium,
+                                color = Color(0xFF252525),
+                                fontSize = 18.sp
+                            )
+                        ) {
+                            append("Olá, Nivea")
+                        }
+                    }
+                )
+            }
+            Image(
+                painter = painterResource(id = R.mipmap.fav),
+                contentDescription = "favoritos",
+                modifier = Modifier.size(40.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.width(250.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = "Home",
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.Bold,
+                        textDecoration = TextDecoration.Underline,
+                        fontSize = 16.sp
+                    ),
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = "Perfil",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Spacer(modifier = Modifier.height(50.dp))
 
+            Column(modifier = Modifier.fillMaxWidth()) {
+                val companiesInColumns = mockCompanies.chunked(2)
+                companiesInColumns.forEach { columnCompanies ->
+                    Row(Modifier.fillMaxWidth()) {
+                        columnCompanies.forEach { company ->
+                            CompanyCard(company)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
 }
 
+@Composable
+fun CompanyCard(company: Company) {
+    Card {
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+                .width(120.dp)
+        ) {
+            Text(company.name, style = MaterialTheme.typography.bodyMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(company.description)
+        }
+    }
+}
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewPlano() {
--- cosmeet/app/src/main/java/com/example/cosmeet/screen/login/Login.kt
+++ cosmeet/app/src/main/java/com/example/cosmeet/screen/login/Login.kt
@@ -33,157 +33,173 @@ import com.example.cosmeet.R
 fun LoginScreen() {
     val isSecondStepVisible = remember { mutableStateOf(false) }
 
-@Composable
-fun FirstStepLoginScreen(onLoginClick: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(modifier = Modifier.width(250.dp), horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically) {
-            Image(painter = painterResource(id = R.mipmap.logo), contentDescription = "logo", modifier = Modifier.size(80.dp))
-            Text(
-                text = buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.Medium,
-                            color = Color(0xFF432D67),
-                            fontSize = 30.sp
-                        )
-                    ) {
-                        append("COSMEET")
+    @Composable
+    fun FirstStepLoginScreen(onLoginClick: () -> Unit) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.width(250.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.mipmap.logo),
+                    contentDescription = "logo",
+                    modifier = Modifier.size(80.dp)
+                )
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Medium,
+                                color = Color(0xFF432D67),
+                                fontSize = 30.sp
+                            )
+                        ) {
+                            append("COSMEET")
+                        }
                     }
-                }
+                )
+
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Bem-vindo(a) ao Cosmeet!",
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
             )
 
-        }
-        Spacer(modifier = Modifier.height(30.dp))
-        Text(
-            text = "Bem-vindo(a) ao Cosmeet!",
-            style = MaterialTheme.typography.labelLarge,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+            Text(
+                text = "Entre com seu e-mail e senha para acessar sua conta.",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
 
-        Text(
-            text = "Entre com seu e-mail e senha para acessar sua conta.",
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(bottom = 24.dp)
-        )
+            OutlinedTextField(
+                value = "E-mail",
+                onValueChange = {},
+                label = { Text("E-mail") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
-        OutlinedTextField(
-            value = "E-mail",
-            onValueChange = {},
-            label = { Text("E-mail") },
-            modifier = Modifier.fillMaxWidth()
-        )
+            Spacer(modifier = Modifier.height(8.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = "Senha",
+                onValueChange = {},
+                label = { Text("Senha") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
 
-        OutlinedTextField(
-            value = "Senha",
-            onValueChange = {},
-            label = { Text("Senha") },
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth()
-        )
+            Spacer(modifier = Modifier.height(24.dp))
 
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(
-            onClick = onLoginClick,
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(Color(0xFF432D67))
-        ) {
-            Text("Entrar")
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(
-            text = buildAnnotatedString {
-                append("Não tem uma conta? ")
-                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                    append("Crie uma agora!")
-                }
-            },
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.clickable { /* TODO: Navegar para tela de cadastro */ }
-        )
-    }
-}
-
-@Composable
-fun SecondStepLoginScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(modifier = Modifier.width(250.dp), horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically) {
-            Image(painter = painterResource(id = R.mipmap.logo), contentDescription = "logo", modifier = Modifier.size(80.dp))
+            Button(
+                onClick = onLoginClick,
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color(0xFF432D67))
+            ) {
+                Text("Entrar")
+            }
+            Spacer(modifier = Modifier.height(20.dp))
             Text(
                 text = buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.Medium,
-                            color = Color(0xFF432D67),
-                            fontSize = 30.sp
-                        )
-                    ) {
-                        append("COSMEET")
+                    append("Não tem uma conta? ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Crie uma agora!")
                     }
-                }
+                },
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.clickable { /* TODO: Navegar para tela de cadastro */ }
             )
         }
-        Text(
-            text = "Verificação em duas etapas",
-            style = MaterialTheme.typography.labelLarge,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        Text(
-            text = "Um código de segurança foi enviado para o seu número de telefone.",
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(bottom = 24.dp)
-        )
-
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Código de segurança") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(
-            onClick = {},
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(Color(0xFF432D67))
-
-        ) {
-            Text("Validar")
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = "Não recebeu o código?",
-            style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.clickable { /* TODO: Reenviar o código */ }
-        )
-
-        Spacer(modifier = Modifier.height(3.dp))
-        Text(
-            text = "Reenviar o código",
-            style = MaterialTheme.typography.titleSmall,
-            modifier = Modifier.clickable { /* TODO: Reenviar o código */ }
-        )
     }
-}
+
+    @Composable
+    fun SecondStepLoginScreen() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.width(250.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.mipmap.logo),
+                    contentDescription = "logo",
+                    modifier = Modifier.size(80.dp)
+                )
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Medium,
+                                color = Color(0xFF432D67),
+                                fontSize = 30.sp
+                            )
+                        ) {
+                            append("COSMEET")
+                        }
+                    }
+                )
+            }
+            Text(
+                text = "Verificação em duas etapas",
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = "Um código de segurança foi enviado para o seu número de telefone.",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Código de segurança") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color(0xFF432D67))
+
+            ) {
+                Text("Validar")
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Não recebeu o código?",
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.clickable { /* TODO: Reenviar o código */ }
+            )
+
+            Spacer(modifier = Modifier.height(3.dp))
+            Text(
+                text = "Reenviar o código",
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.clickable { /* TODO: Reenviar o código */ }
+            )
+        }
+    }
 
 
     Column(
