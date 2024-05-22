d0b2030df87049333f7a7ee5abd18ef2722139e9
kanaiama <wilson.kanaiamajr@sptech.school>
2024-05-22 19:05:56 -0300

    att

--- cosmeet/app/src/main/java/com/example/cosmeet/MainActivity.kt
+++ cosmeet/app/src/main/java/com/example/cosmeet/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -32,7 +34,7 @@ import com.example.cosmeet.ui.theme.CosmeetTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        installSplashScreen()
+        installSplashScreen()
         setContent {
             CosmeetTheme {
                 val navController = rememberNavController()
@@ -102,7 +104,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Text(text = "teste")
 }
 
 @Preview(showBackground = true, showSystemUi = true)
